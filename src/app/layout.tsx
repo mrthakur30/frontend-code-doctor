273a86e67e9005e64494c02267d2e41fe6081dce
Mukul Thakur <95705739+mrthakur30@users.noreply.github.com>
2023-07-27 01:20:41 +0530

    third commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import '../styles/globals.css'
 import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
 
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -14,8 +16,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head><title>XEROCODE</title></head>
-      <body>{children}</body>
+      <head> <title>XEROCODE</title></head>
+      <body className={inter.className}>{children}</body>
     </html>
   )
 }
